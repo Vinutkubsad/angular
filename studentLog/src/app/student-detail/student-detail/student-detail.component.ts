540fb4e42c8c585f5a848820583805ab8c7a4851
Vinutkubsad <vinutkumar.kubsad@accionlabs.com>
2018-08-17 17:33:51 +0530

    studentLog

--- studentLog/src/app/student-detail/student-detail/student-detail.component.ts
+++ studentLog/src/app/student-detail/student-detail/student-detail.component.ts
@@ -11,13 +11,56 @@ import { FormControl } from '@angular/forms';
 export class StudentDetailComponent implements OnInit {
 
   //studentDetails: FormGroup;
+  showForm = false;
+ public studentdetail:student=new student();
+ public education:education=new education();
+ 
+  constructor() { 
 
-  constructor(private rou:Router) { 
   }
 
+  optionsSelect: Array<any>;
   ngOnInit() {
-  }
+ }
   onNext = function(){
-    this.rou.navigateByUrl ('/edu-details');
+    this.showForm = true;
+    //this.rou.navigateByUrl ('/edu-details');
   }
+  change(e){
+   this. education.list=e;
+  }
+  
 }
+export class student{
+  firstName:string;
+  middlename:string;
+  lastname:string;
+  email:string;
+  phone:number;
+  adress:string;
+  adress2:string;
+  city:string;
+  zip:number;
+  check1: string;
+  check2: string;
+  check3: string;
+}
+export class education{
+  b1:string;
+  b2:string;
+  b3:string;
+
+  c1:string;
+  c2:string;
+  c3:string;
+
+  d1:string;
+  d2:string;
+  d3:string;
+
+  e1:string;
+  e2:string;
+  e3:string;
+  list:string;
+}
+
